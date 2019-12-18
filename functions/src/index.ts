d8499182d4278553eac374db37e02a873051bc5c
cesarvega <cesarvega.col@gmail.com>
2019-12-18 11:37:14 -0500

    commit to pull

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,3 @@
-// import * as functions from 'firebase-functions';
-
-
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
@@ -11,41 +8,41 @@ const stripe = require('stripe')(functions.config().stripe.testkey)
 
 
 exports.stripeCharge = functions.database
-                                .ref('/payments/{userId}/{paymentId}')
-                                .onWrite((change: any,context: any)=> {
-                                    const payment = change.after.val();
-                                    const userId = context.params.userId;
-                                    const paymentId = context.params.paymentId;
-  
-
-  // checks if payment exists or if it has already been charged
-  if (!payment || payment.charge) return;
-
-  return admin.database()
-              .ref(`/users/${userId}`)
-              .once('value')
-              .then((snapshot: any) => {
-                  return snapshot.val();
-               })
-               .then((customer: any) => {
-
-                 const amount = payment.amount;
-                 const idempotency_key = paymentId;  // prevent duplicate charges
-                 const source = payment.token.id;
-                 const currency = 'usd';
-                 const charge = {amount, currency, source};
+  .ref('/payments/{userId}/{paymentId}')
+  .onWrite((change: any, context: any) => {
+    const payment = change.after.val();
+    const userId = context.params.userId;
+    const paymentId = context.params.paymentId;
 
 
-                 return stripe.charges.create(charge, { idempotency_key });
+    // checks if payment exists or if it has already been charged
+    if (!payment || payment.charge) return;
 
-               })
+    return admin.database()
+      .ref(`/users/${userId}`)
+      .once('value')
+      .then((snapshot: any) => {
+        return snapshot.val();
+      })
+      .then((customer: any) => {
 
-               .then((charge: any) => {
-                admin.database()
-                .ref(`/payments/${userId}/${paymentId}/charge`)
-                .set(charge);
-                return true;
-                })
+        const amount = payment.amount;
+        const idempotency_key = paymentId;  // prevent duplicate charges
+        const source = payment.token.id;
+        const currency = 'usd';
+        const charge = { amount, currency, source };
 
 
-});
+        return stripe.charges.create(charge, { idempotency_key });
+
+      })
+
+      .then((charge: any) => {
+        admin.database()
+          .ref(`/payments/${userId}/${paymentId}/charge`)
+          .set(charge);
+        return true;
+      })
+
+
+  });
